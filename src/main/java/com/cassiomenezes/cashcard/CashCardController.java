3257d0f5f5f781094f6971dda1542fbe1a909940
Cassio Menezes <cassio.menezes@outlook.com.br>
2024-10-15 17:31:09 -0300

    feat: using @PathVariable

--- src/main/java/com/cassiomenezes/cashcard/CashCardController.java
+++ src/main/java/com/cassiomenezes/cashcard/CashCardController.java
@@ -2,6 +2,7 @@ package com.cassiomenezes.cashcard;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CashCardController {
 
 	@GetMapping("/{requestedId}")
-	private ResponseEntity<CashCard> findById() {
+	private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
 
-		CashCard cashCard = new CashCard(99L, 123.45);
-		return ResponseEntity.ok(cashCard);
+		if (requestedId.equals(99L)) {
+			CashCard cashCard = new CashCard(99L, 123.45);
+			return ResponseEntity.ok(cashCard);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+
 	}
 }
