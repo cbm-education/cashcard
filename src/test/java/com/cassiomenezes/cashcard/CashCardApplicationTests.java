c5764cef0cac2c237c7731955f007daac101cbd6
Cassio Menezes <cassio.menezes@outlook.com.br>
2024-10-15 17:01:14 -0300

    tests: test amount

--- src/test/java/com/cassiomenezes/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/cassiomenezes/cashcard/CashCardApplicationTests.java
@@ -31,6 +31,9 @@ class CashCardApplicationTests {
 
 		Number id = documentContext.read("$.id");
 		assertThat(id).isEqualTo(99);
+
+		Double amount = documentContext.read("$.amount");
+		assertThat(amount).isEqualTo(123.45);
 	}
 
 }
