1569555fe41a212e0bb8401088fc5915747da353
Cassio Menezes <cassio.menezes@outlook.com.br>
2024-10-15 16:32:13 -0300

    tests: failing test for get

--- src/test/java/com/cassiomenezes/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/cassiomenezes/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,29 @@
 package com.cassiomenezes.cashcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
+
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcard/99", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
