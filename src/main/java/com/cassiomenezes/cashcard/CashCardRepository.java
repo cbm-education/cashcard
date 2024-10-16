3db6b13301c3b0924150a2cdab643294712ea50a
Cassio Menezes <cassio.menezes@outlook.com.br>
2024-10-16 17:47:51 -0300

    feat: preparing for data

--- src/main/java/com/cassiomenezes/cashcard/CashCardRepository.java
+++ src/main/java/com/cassiomenezes/cashcard/CashCardRepository.java
@@ -1,7 +1,6 @@
 package com.cassiomenezes.cashcard;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.CrudRepository;
 
-record CashCard(@Id Long id, Double amount) {
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
 }
