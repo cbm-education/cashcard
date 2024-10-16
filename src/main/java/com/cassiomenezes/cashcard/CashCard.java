420c4f7f0327ec0a79fd5cb3f6eb997d46456647
Cassio Menezes <cassio.menezes@outlook.com.br>
2024-10-16 17:50:28 -0300

    housekeeping

--- src/main/java/com/cassiomenezes/cashcard/CashCard.java
+++ src/main/java/com/cassiomenezes/cashcard/CashCard.java
@@ -1,7 +1,6 @@
 package com.cassiomenezes.cashcard;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.repository.CrudRepository;
 
 record CashCard(@Id Long id, Double amount) {
 }
