18afcc126d1efe21df4c22b769a1a74bafbc0058
Cassio Menezes <cassio.menezes@outlook.com.br>
2024-10-23 17:25:44 -0300

    feat: configuring basic authentication

--- src/main/java/com/cassiomenezes/cashcard/SecurityConfig.java
+++ src/main/java/com/cassiomenezes/cashcard/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cassiomenezes.cashcard;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,6 +11,12 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests(request -> request
+						.requestMatchers("/cashcards/**")
+						.authenticated())
+				.httpBasic(Customizer.withDefaults())
+				.csrf(csrf -> csrf.disable());
 		return http.build();
 	}
 }
