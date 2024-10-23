e33cbcfa3200b7834e5761c309215e2831ead442
Cassio Menezes <cassio.menezes@outlook.com.br>
2024-10-23 19:06:45 -0300

    feat: implementing basic authentication

--- src/main/java/com/cassiomenezes/cashcard/SecurityConfig.java
+++ src/main/java/com/cassiomenezes/cashcard/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +24,22 @@ public class SecurityConfig {
 				.httpBasic(Customizer.withDefaults())
 				.csrf(csrf -> csrf.disable());
 		return http.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return  new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+		User.UserBuilder users = User.builder();
+		UserDetails sarah = users
+				.username("sarah1")
+				.password(passwordEncoder.encode("hu@!s6G-0"))
+				.roles() // No roles for now
+				.build();
+
+		return new InMemoryUserDetailsManager(sarah);
 	}
 }
