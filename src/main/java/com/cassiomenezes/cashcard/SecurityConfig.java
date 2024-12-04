c20b3b2178f0c79853c9f70d30e615d4b5586ff7
Cassio Menezes <cassio.menezes@outlook.com.br>
2024-12-04 08:44:00 -0300

    feat: implementing security control

--- src/main/java/com/cassiomenezes/cashcard/SecurityConfig.java
+++ src/main/java/com/cassiomenezes/cashcard/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,12 +18,11 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-				.authorizeHttpRequests(request -> request
-						.requestMatchers("/cashcards/**")
-						.hasRole("CARD-OWNER"))
-				.httpBasic(Customizer.withDefaults())
-				.csrf(csrf -> csrf.disable());
+		final HttpSecurity httpSecurity = http.authorizeHttpRequests(request -> request
+				.requestMatchers("/cashcards/**")
+				.hasRole("CARD-OWNER"));
+		http.httpBasic(Customizer.withDefaults());
+		http.csrf(AbstractHttpConfigurer::disable);
 		return http.build();
 	}
 
